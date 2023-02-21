6ffc7d4487d28705a9b50d69165ddfc731ca172f
seohyun9672 <ajeong4@my.bcit.ca>
2023-02-20 18:41:46 -0800

    add condition

--- comp/Card.js
+++ comp/Card.js
@@ -7,19 +7,25 @@ export default function Card(props) {
       <div className="card-content">
         <div className="card-title">{props.title}</div>
         <div className="detail-cont">
-          <div className="detail">
-            <img src="/imgs/star.svg" />
-            <div>{props.ratings}</div>
-          </div>
-          <div className="detail">
-            <img src="/imgs/servings.svg" />
-            <div>{props.servings}</div>
-            <div>{props.unit}</div>
-          </div>
-          <div className="detail">
-            <img src="/imgs/time.svg" />
-            <div>{props.time}</div>
-          </div>
+          {props.ratings !== "" && (
+            <div className="detail">
+              <img src="/imgs/star.svg" />
+              <div>{props.ratings}</div>
+            </div>
+          )}
+          {props.servings !== "" && (
+            <div className="detail">
+              <img src="/imgs/servings.svg" />
+              <div>{props.servings}</div>
+              <div>{props.unit}</div>
+            </div>
+          )}
+          {props.time !== "" && (
+            <div className="detail">
+              <img src="/imgs/time.svg" />
+              <div>{props.time}</div>
+            </div>
+          )}
         </div>
       </div>
     </div>
